cae3cef56b1fad0ae229a208c0903b9f5c63e806
AdielsonMedeiros <medeirosadielson@gmail.com>
2025-08-30 23:27:44 -0300

    Fix: Ajusta o caminho base para deploy na Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/meu-projeto-allsafe/', // <-- MUITO IMPORTANTE: Coloque o nome do seu repositório aqui
+  base: '/', 
 })
